1150c9ca9a8b66b222f8db8be5636e6caafdf904
Yum <yunisio@gmail.com>
2016-05-31 11:35:34 +0300

    GameManager (#03 video)

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    BoardManager boardScript;
+
+    private int level = 3;
+
+	void Awake()
+    {
+        boardScript = GetComponent<BoardManager>();
+        InitGame();
+	
+	}
+
+    private void InitGame()
+    {
+        boardScript.SetupScene(level);
+    }
+}
